14865d928dad453c28cb9c1a6322c8db5a88ab48
Darth Vader <darth091695vader@gmail.com>
2021-01-21 14:09:01 +0800

    deploy my react

---
+++ src/components/SectionOne/SectionOne.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './SectionOne.css'
+
+const sectionOne = () => {
+    return (
+        <section>
+            <div id='one' className='jumbotron'>
+                <div className='container'>
+                <h1 className='one-title mb-5 headline mh'>Hi, my name is <span style={{color:'#00cdac'}}>Alvin Paala</span><br />
+                I'm a Front-end Developer.</h1>
+                <p className='know-more headline-button mh-button'>
+                    <div class="button" id="button-2">
+                        <div id="slide"></div>
+                        <a href="#two"><b>Know more</b></a>
+                    </div>
+                </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default sectionOne
